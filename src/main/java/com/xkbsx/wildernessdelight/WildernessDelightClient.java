fc91bc4255a23ec87dbeb3eccfcef4a2eb678834
Xkbsx413 <xkbsx@qq.com>
2026-05-31 19:28:17 +0800

    测试

---
+++ src/main/java/com/xkbsx/wildernessdelight/WildernessDelightClient.java
@@ -0,0 +1,10 @@
+package com.xkbsx.wildernessdelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WildernessDelightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
